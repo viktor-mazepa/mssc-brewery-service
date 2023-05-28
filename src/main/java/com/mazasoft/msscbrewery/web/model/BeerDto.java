898e7db714a934271622ddb351e8d7468ea51ffd
vima1013 <viktor.mazepa@NetCracker.com>
2023-05-28 16:24:28 +0300

    Add model classes for Beer

---
+++ src/main/java/com/mazasoft/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,33 @@
+package com.mazasoft.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID uuid;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+
+    private String beerName;
+
+    private BeerStyleEnum beerStyle;
+
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/com/mazasoft/msscbrewery/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.mazasoft.msscbrewery.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
