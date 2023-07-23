85c4b96cbe7384f8c209055de6d4aab724d3a615
vima1013 <viktor.mazepa@NetCracker.com>
2023-07-23 16:09:58 +0300

    Added validation implementation

--- src/main/java/com/mazasoft/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/mazasoft/msscbrewery/web/controller/BeerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -35,13 +36,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity saveNewBeer(@RequestBody @Valid BeerDto beerDto) {
         beerService.saveNewBeer(convertToEntity(beerDto));
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Valid BeerDto beerDto) {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
