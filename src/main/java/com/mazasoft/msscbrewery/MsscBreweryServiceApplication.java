043078c8b899e2a570b4aaa1e925c9edf6515f02
viktor-mazepa <viktor.mazepa@gmail.com>
2023-10-01 17:00:46 +0300

    Fix hibernate connection errors

--- src/main/java/com/mazasoft/msscbrewery/MsscBreweryServiceApplication.java
+++ src/main/java/com/mazasoft/msscbrewery/MsscBreweryServiceApplication.java
@@ -2,8 +2,9 @@ package com.mazasoft.msscbrewery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class MsscBreweryServiceApplication {
 
     public static void main(String[] args) {
